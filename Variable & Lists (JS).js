3d5e1e0eb432c85a5c12992ae170c7e07031c1da
Kabir Sharma <sharmak2@tcd.ie>
2025-09-25 14:26:59 +0100

    Add files via upload

---
+++ Variable & Lists (JS).js
@@ -0,0 +1,17 @@
+
+let name = "Kabir";
+let age = 22;
+let isStudent = true;
+let city = "Dublin";
+let course = "Computer Science";
+
+
+age = 23;             // changed from 22
+city = "Cork";        // changed from Dublin
+
+
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+// Step 4: List the second and third items
+console.log("Second item:", fruits[1]);  // Banana
+console.log("Third item:", fruits[2]);   // Mango
