e9a81de66b8f2d5fe15d4ac10cdfc3ea2c5fc683
Kabir Sharma <sharmak2@tcd.ie>
2025-09-25 14:52:45 +0100

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,17 @@
+
+let playerScore = 0;
+
+
+function addPoint() {
+  playerScore = playerScore + 1; 
+}
+
+
+function displayScore() {
+  console.log("Player Score:", playerScore);
+}
+
+
+addPoint();       
+addPoint();      
+displayScore();   
